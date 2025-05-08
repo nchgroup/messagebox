31f3f4bce9b0749324d7f68a360063f83185df09
Vay3t <fffyanez@gmail.com>
2025-05-08 12:10:52 -0400

    Update main-dll.c

--- main-dll.c
+++ main-dll.c
@@ -1,7 +1,7 @@
 #include <windows.h>
 
 extern __declspec(dllexport) void ShowMessage(){
-    MessageBox(NULL, "Test", "Msg test", MB_OK | MB_ICONINFORMATION);
+    MessageBox(NULL, "Msg test", "Test", MB_OK | MB_ICONINFORMATION);
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved){
