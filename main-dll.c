5ded1348b58fa508c47ed1721ec2c6236a33bf7c
Vay3t <fffyanez@gmail.com>
2025-05-08 11:46:38 -0400

    Create main-dll.c

---
+++ main-dll.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+extern __declspec(dllexport) void ShowMessage(){
+    MessageBox(NULL, "Test", "Msg test", MB_OK | MB_ICONINFORMATION);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
