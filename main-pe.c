546810b81886d9603e99811ea32a4210572c72ea
Vay3t <fffyanez@gmail.com>
2025-05-08 12:11:01 -0400

    Update main-pe.c

--- main-pe.c
+++ main-pe.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 
 int main(){
-    MessageBox(NULL, "Test", "Msg test", MB_OK | MB_ICONINFORMATION);
+    MessageBox(NULL, "Msg test", "Test", MB_OK | MB_ICONINFORMATION);
     return 0;
 }
