bafc0dafc383b5c9e8c274b20eba4ca70e531d82
Vay3t <fffyanez@gmail.com>
2025-05-18 19:01:51 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-x86_64-w64-mingw32-gcc -Os -flto -fdata-sections -ffunction-sections -Wl,--gc-sections -s -mwindows -o test.exe main.c
+x86_64-w64-mingw32-gcc -Os -flto -fdata-sections -ffunction-sections -Wl,--gc-sections -s -mwindows -o test.exe main-pe.c
+x86_64-w64-mingw32-gcc -shared -O2 -s -ffunction-sections -fdata-sections -fmerge-all-constants -static-libgcc -o test.dll main-dll.c
